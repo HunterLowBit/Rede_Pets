adafbfc16c85fee82302cb778e5e017bea383b41
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-07 18:25:36 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const Handlebars = require('express-handlebars');
 const app = express();
+const handlebars = require('express-handlebars');
 
-app.engine('handlebars', Handlebars({defaultlayout: 'main'}));
-app.set('view engine', 'handlebars');
+  app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+  app.set('view engine', 'handlebars');
 
+//Rotas
+app.get('/', function(req, res){
+  res.render('index')
+})
 
 app.listen(3000, function() {
   console.log('O servidor está rodando na porta 3000\n\nhttp://localhost:3000/');

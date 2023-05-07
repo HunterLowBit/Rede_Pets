249c614d00a358a24a6441ab6f76a21fa31b51c0
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-07 18:00:35 -0300

    Reorganizar

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const Handlebars = require('express-handlebars');
 const app = express();
 
-app.engine('handlebars', Handlebars({defaultlayout}));
+app.engine('handlebars', Handlebars({defaultlayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/html/index.html');
-});
 
 app.listen(3000, function() {
   console.log('O servidor está rodando na porta 3000\n\nhttp://localhost:3000/');

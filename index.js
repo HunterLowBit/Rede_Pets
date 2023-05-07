1fac747097457cf9abce47dd8a57813a1288d4f0
Robson <53387930+HunterLowBit@users.noreply.github.com>
2023-05-07 17:38:11 -0300

    Organização de diretorio + nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/html/index.html');
 });
 
 app.listen(3000, function() {
